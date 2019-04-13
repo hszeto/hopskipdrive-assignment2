aab17a52f50dd952eb73e8b2059d26819c4a221b
Henry Szeto <henryszeto@gmail.com>
2019-04-13 09:54:29 -0700

    validates_presence_of :frequency, :days, :time, :location

--- app/models/repeating_ride.rb
+++ app/models/repeating_ride.rb
@@ -6,6 +6,8 @@ class RepeatingRide < ApplicationRecord
   belongs_to :user
   has_many :rides, dependent: :destroy
 
+  validates_presence_of :frequency, :days, :time, :location
+
   def update_rides(data)
     self.rides.update(data)
   end
