d1bdfabcf1b8b0e76ae507d44a5aea0df36a085f
Henry Szeto <henryszeto@gmail.com>
2019-04-14 11:19:49 -0700

    Module for json response

---
+++ app/controllers/concerns/response_handler.rb
@@ -0,0 +1,25 @@
+module ResponseHandler
+  extend ActiveSupport::Concern
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def json_repeating_ride_response(status = :ok)
+    render json: selected_attributes_for_repeating_ride, status: status
+  end
+
+  def selected_attributes_for_repeating_ride
+    @repeating_ride.to_json(
+      only: [:frequency, :location, :time, :days],
+      include: [
+        user: {
+          only:[:email]
+        },
+        rides: {
+          only:[:date, :time, :location]
+        }
+      ]
+    )
+  end
+end
